9625fa4a508dbb6ea810eefc129f493395cc2257
Evan The Author <161684804+EvansTheAuthor@users.noreply.github.com>
2024-09-03 14:57:44 +0700

    Update artis.java

--- pertemuan3/abstractVSinterface/Interface/artis.java
+++ pertemuan3/abstractVSinterface/Interface/artis.java
@@ -1,6 +1,11 @@
 package pertemuan3.abstractVSinterface.Interface;
 
 class artis implements komikus, penyanyi{
+    /*
+    * karena artis itu bisa multi talenta
+     * dan menyanyi bisa banyak lagu
+     * atau menggambar bisa berbagai judul komik 
+    */
     //void yang diimplementasikan dari komikus.java
     public void menggambar(){
         System.out.println("Artis sedang menggambar komik");
